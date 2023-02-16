a130528c6bfd4b85b7e58a7cd9913a2a36cf9a1b
Linnovel <luislinnovel@gmail.com>
2023-02-16 02:38:12 +0000

    Mi primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const agregarTarea = (e) => {
+    e.preventDefault();
+    if (inputValue.length === 0) return;
+    setTodos(todos.concat(inputValue));
+    setInputValue("");
+  };
+
+  return (
+    <div className="container">
+      <h1>My ToDo List</h1>
+      <ul>
+        <li style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                agregarTarea(e);
+              }
+            }}
+            placeholder="Give me New Task!"
+          />
+          <i
+            className="fas fa-plus"
+            onClick={agregarTarea}
+            style={{ cursor: "pointer", marginLeft: "0.5em" }}
+          ></i>
+        </li>
+        {todos.map((item, index) => (
+          <li key={index}>
+            {item}{" "}
+            <i className="fas fa-trash-alt delete-button" onClick={() =>
+                setTodos(todos.filter((t, currentIndex) => index !== currentIndex))
+              }
+            ></i>{" "}
+          </li>
+        ))}
+      </ul>
+      <div>{todos.length ? `${todos.length} tasks` : "No pending list, add more?" }</div>
+    </div>
+  );
 };
 
 export default Home;
